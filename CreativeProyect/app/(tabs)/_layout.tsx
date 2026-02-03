624edf8aeba51338f140489b7727ed4005908110
CesarAybar <20240096@itla.edu.do>
2026-02-03 19:29:32 -0400

    added sumadora

---
+++ CreativeProyect/app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router"
+
+export default function TabLayout(){
+    return (
+        <Tabs>
+            <Tabs.Screen name="index" options={{title: 'Home'}}/>
+            <Tabs.Screen name = "sumadora" options={{title: 'Sumadora'}}/>
+        </Tabs>
+    )
+}
